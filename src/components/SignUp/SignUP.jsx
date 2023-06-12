db047cd94cdbd9a0aab10c4b89248d01109f5ed2
HamzaAliKUndi <hamza.alee83@gmail.com>
2023-06-12 23:11:03 +0500

    Working on SignUP

--- src/components/SignUp/SignUP.jsx
+++ src/components/SignUp/SignUP.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "./signup.css";
 
 const SignUP = () => {
   const navigate = useNavigate();
@@ -11,7 +12,24 @@ const SignUP = () => {
   }, [navigate]);
   return (
     <>
-      <h1>SignUP</h1>
+      <div className="banner d-flex justify-content-center">
+        <div>
+          <div>
+            <span className="title">Register for Disease</span>
+          </div>
+          <div className="underTitle">
+            <span>Detection</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="container bg-info">
+        <div className="row">
+          <div className="col-lg-6">23</div>
+          <div className="col-lg-6">213</div>
+        </div>
+      </div>
+      <h1>Header</h1>
     </>
   );
 };

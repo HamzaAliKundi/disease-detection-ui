4cc356b1106b2014da0e78b9b0cba7662ddc859d
HamzaAliKUndi <hamza.alee83@gmail.com>
2023-06-12 14:08:03 +0500

    Set Home Page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./home.css";
 
 const Home = () => {
@@ -6,9 +7,17 @@ const Home = () => {
     <>
       <div className="wrapper">
         <div className="title">Welcome to Heart Disease Detection</div>
-        <div className="middleImage">
-          <img src="../Assets/Images/HOmePageMiddleImage.png" alt="" />
+        <div className="imageWrapper">
+          <div className="middleImage"></div>
         </div>
+        <div className="subTitle">Want To Try Out Our Prediction Model? </div>
+        <Link to="/login" className="buttonWrapper">
+          <div className="button">
+            <div>
+              <span className="textDesign"> Let's Predict {">"} </span>
+            </div>
+          </div>
+        </Link>
       </div>
     </>
   );

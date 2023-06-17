e28c9f9caeb5a23a7800a6143cdef908c0335dd9
HamzaAliKUndi <hamza.alee83@gmail.com>
2023-06-17 20:36:59 +0500

    Set ContactUs Page

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "./about.css";
 
 const AboutUs = () => {
   const navigate = useNavigate();
@@ -11,7 +12,25 @@ const AboutUs = () => {
   }, [navigate]);
   return (
     <>
-      <h1> AboutUs</h1>
+      <div className="titleAboutUs">About Us</div>
+      <div className="infoAboutUs">
+        our mission is to improve the health and well-being of individuals by
+        harnessing the power of artificial intelligence and predictive
+        analytics. We have developed a cutting-edge Heart Disease Prediction
+        System that utilizes state-of-the -art machine learning algorithms to
+        accurately assess the risk of heart disease in individuals.
+      </div>
+      <div className="ourMissionTitle">Our Mission</div>
+      <div className="infoOurMission">
+        Our dedicated team of data scientists, healthcare professionals, and
+        software engineers have combined their expertise to create a robust and
+        reliable system that empowers users to proactively manage their
+        cardiovascular health.
+      </div>
+      <div className="aboutUsWrapper">
+        <div className="aboutUs"></div>
+      </div>
+      <div className="footer"></div>
     </>
   );
 };

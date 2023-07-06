b69d894f1862501a883fe764b15beb7bbf0a254d
HamzaAliKUndi <hamza.alee83@gmail.com>
2023-07-06 10:03:10 +0500

    set User Account Page

--- src/components/UserAccount/UserAccount.jsx
+++ src/components/UserAccount/UserAccount.jsx
@@ -2,12 +2,12 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate, useParams } from "react-router-dom";
 import userAccount from "../Assets/Images/userAccountPic.png";
+import jwtDecode from "jwt-decode";
 import "./user.css";
 
 const UserAccount = () => {
   const params = useParams();
   const predict = params.id;
-  console.log("Predicst Parama : ", predict);
 
   const navigate = useNavigate();
   useEffect(() => {
@@ -16,6 +16,11 @@ const UserAccount = () => {
       navigate("/");
     }
   }, [navigate]);
+
+  const token = localStorage.getItem("token");
+  const user = jwtDecode(token);
+
+  console.log("User : ", user);
   return (
     <>
       <div className="header">
@@ -45,57 +50,59 @@ const UserAccount = () => {
                     1
                   </th>
                   <td>Full Name</td>
-                  <td>Hamza Ali Kundi</td>
+                  <td>{user.fullName}</td>
                 </tr>
                 <tr>
                   <th scope="row" className="tableHeaderTextDesign">
                     2
                   </th>
                   <td>Username</td>
-                  <td>hamza.alee83@gamil.com</td>
+                  <td>{user.userName}</td>
                 </tr>
                 <tr>
                   <th scope="row" className="tableHeaderTextDesign">
                     3
                   </th>
                   <td>Age</td>
-                  <td>23</td>
+                  <td>{user.age}</td>
                 </tr>
                 <tr>
                   <th scope="row" className="tableHeaderTextDesign">
                     4
                   </th>
                   <td>Gender</td>
-                  <td>Male</td>
+                  <td>{user.gender}</td>
                 </tr>
                 <tr>
                   <th scope="row" className="tableHeaderTextDesign">
                     5
                   </th>
                   <td>Email</td>
-                  <td>hamza.alee83@gamil.com</td>
+                  <td>{user.email}</td>
                 </tr>
                 <tr>
                   <th scope="row" className="tableHeaderTextDesign">
                     6
                   </th>
                   <td>City</td>
-                  <td>Mianwali</td>
+                  <td>{user.city}</td>
                 </tr>
                 <tr>
                   <th scope="row" className="tableHeaderTextDesign">
                     7
                   </th>
                   <td>Phone</td>
-                  <td>+92 304 9068858</td>
+                  <td>{user.phone}</td>
                 </tr>
                 <tr>
-                  <th scope="row" className="tableHeaderTextDesign">
+                  <th scope="row" className="tableHeaderTextDesign bg-info">
                     8
                   </th>
-                  <td className="text-danger">Disease Stage</td>
-                  <td>
-                    <b>{predict}</b>
+                  <td className="text-danger bg-info text-dark mt-2">
+                    <b> Disease Stage</b>
+                  </td>
+                  <td className="bg-info">
+                    <b style={{ fontSize: "25px" }}>{predict}</b>
                   </td>
                 </tr>
               </tbody>
